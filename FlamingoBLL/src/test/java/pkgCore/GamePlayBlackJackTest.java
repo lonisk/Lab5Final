2c0d6618aaa9f553b3036d1024ca614e8e7b0898
rongsong <rongsong@rongs-macbook.fios-router.home>
2017-11-15 01:47:15 -0500

    GamePlayBlackJackTest initial commit

--- FlamingoBLL/src/test/java/pkgCore/GamePlayBlackJackTest.java
+++ FlamingoBLL/src/test/java/pkgCore/GamePlayBlackJackTest.java
@@ -3,6 +3,8 @@ package pkgCore;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -13,7 +15,19 @@ public class GamePlayBlackJackTest {
 
 	@Test
 	public void TestPlayerWinning() {
-		
+		Table t=new Table();
+		Player p1=new Player("Mike",1);
+		Player p2=new Player("John",2);
+		t.AddPlayerToTable(p1);
+		t.AddPlayerToTable(p2);
+		Deck d=new Deck();
+		GamePlayBlackJack gpBJ=new GamePlayBlackJack(t.getHmTablePlayer(),d);
+		Iterator it=gpBJ.getHmGameHands().entrySet().iterator();
+		while(it.hasNext()) {
+			Map.Entry pair=(Map.Entry)it.next();
+			HandBlackJack hBJ=(HandBlackJack) pair.getValue();
+			GamePlayerHand GPH=(GamePlayerHand)pair.getKey();
+		}
 	}
 
 	@Test
