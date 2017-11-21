44bbbae32305271ddd02d10efb99cf36f136f245
lonisk <lonisk@wifi-roaming-128-4-140-227.host.udel.edu>
2017-11-21 13:02:14 -0500

    done*?

--- FlamingoBLL/src/test/java/pkgCore/UtilTest.java
+++ FlamingoBLL/src/test/java/pkgCore/UtilTest.java
@@ -11,22 +11,17 @@ public class UtilTest {
 	@Test
 	public void TestRemove() {
 		
-		LinkedList<Integer> listI = new LinkedList<Integer>();
-		listI.add(1);
-		listI.add(10);
-		listI.add(21);
-		listI.add(22);
-		listI.add(32);
+		LinkedList<Integer> LIST = new LinkedList<Integer>();
+				LIST.add(1);
+				LIST.add(10);
+				LIST.add(21);
+				LIST.add(22);
+				LIST.add(32);
 		
-		assertEquals(5,listI.size());
-		
-		LinkedList<Integer> filteredList = GamePlayBlackJack.ValidScores(listI);
-		
-		assertEquals(3,filteredList.size());
-		
-		assertEquals(1,filteredList.getFirst().intValue());
-		assertEquals(21,filteredList.getLast().intValue());
-		
+		assertEquals(5,LIST.size());
+		LinkedList<Integer> filLIST = GamePlayBlackJack.ValidScores(LIST);
+		assertEquals(3,filLIST.size());
+		assertEquals(1,filLIST.getFirst().intValue());
+		assertEquals(21,filLIST.getLast().intValue());
 	}
-
 }
